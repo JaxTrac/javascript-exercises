cd6e879f6a4f4342a26c22d35044d1c801403ab3
JaxTrac <116666046+JaxTrac@users.noreply.github.com>
2023-01-19 18:26:41 +0100

    Solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let multiString = "";
 
-};
+    if (num >= 0) {
+        for (i=0; i < num; i++){
+            multiString += string
+        }
+        return multiString;
+    } else {
+        return "ERROR";
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
