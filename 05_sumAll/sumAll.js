e9ad579ed415f8f7a726b11a991414a3825aed2a
JaxTrac <116666046+JaxTrac@users.noreply.github.com>
2023-02-07 15:38:08 +0100

    Create exceptions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,28 @@
 const sumAll = function(startNum, endNum) {
-    let sumOfNums = undefined;
-    for (i=startNum; i<=endNum; i++){
-        sumOfNums += i;
+    if (isNaN(startNum) || isNaN(endNum)){
+        return "ERROR";
+    } else if (startNum < 0 || endNum < 0){
+        return "ERROR";
+    } else if (typeof startNum === "string" || typeof endNum === "string"){
+        return "ERROR";
+    }else {
+        let sumOfNums = 0;
+        if (startNum<=endNum){
+            for (i=startNum; i<=endNum; i++){
+                sumOfNums += i;
+            }
+            console.log(typeof endNum)
+            return sumOfNums;
+        } else {
+            for (i=endNum; i<=startNum; i++){
+                sumOfNums += i;
+            }
+            
+            return sumOfNums;
+        }
     }
-    return sumOfNums;
 };
-
+console.log(sumAll(1,"345"));
+console.log(typeof "123")
 // Do not edit below this line
 module.exports = sumAll;
