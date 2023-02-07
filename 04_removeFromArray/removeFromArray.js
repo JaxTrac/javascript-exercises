45b216215f17a681ad1a12bba958fe59b521fc7f
JaxTrac <116666046+JaxTrac@users.noreply.github.com>
2023-02-07 14:15:08 +0100

    Solution with True/False approach

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
 const removeFromArray = function() {
-
+   let toBeShortenedArray = arguments[0];
+   for (i=1; i <= arguments.length; i++) {
+    if (toBeShortenedArray.includes(arguments[i])){
+        for (a=0; a<=toBeShortenedArray.length; a++){
+            if (toBeShortenedArray[a]==arguments[i]){
+                toBeShortenedArray.splice(a,1);
+                a=-1;
+            }
+        }
+    }}
+    return toBeShortenedArray
 };
 
 // Do not edit below this line
