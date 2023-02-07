d1a54cb5181db7ea554df9e2a24ed140f190d6af
JaxTrac <116666046+JaxTrac@users.noreply.github.com>
2023-02-07 15:46:25 +0100

    Solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+// if divided by 400 -> leapyear
+    if (year%400==0){
+        return true;
+// if divided by 100 -> no leapyear
+    } else if (year%100==0){
+        return false;
+// if divided by 4 -> leapyear
+    } else if (year%4==0){
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
