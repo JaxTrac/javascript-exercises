7101645e0ad743ea10a94b5d77fefa61bde9a4a3
JaxTrac <116666046+JaxTrac@users.noreply.github.com>
2023-07-05 18:29:03 +0200

    Solve problem 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
+const add = function(a,b) {
+  return a + b;
+};
+
+const subtract = function(a,b) {
+  return a - b;
+};
+
+const sum = function(array) {
+  return array.reduce(function(accumulator, currentValue){
+    return accumulator + currentValue;
+  }, 0); 
+  
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const multiply = function(array) {
+  return array.reduce(function(accumulator, currentValue){
+    return accumulator * currentValue;
+  }, 1); 
 
 };
 
-const power = function() {
+const power = function(a,b) {
+  return a ** b;
 	
 };
 
-const factorial = function() {
-	
+const factorial = function(factorial) {
+  let currentValue = 1
+  for (let i = factorial; i > 0; i--) {
+    currentValue = currentValue * i;
+  }
+	return currentValue;
 };
 
 // Do not edit below this line
