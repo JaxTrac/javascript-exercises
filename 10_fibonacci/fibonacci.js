007caa945f81aa8fb5bbc34d0676924ddff3c55e
JaxTrac <116666046+JaxTrac@users.noreply.github.com>
2023-07-06 17:38:49 +0200

    Solve fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let value = [1,1];
+    if (number > 0) {
+        for (let i = 2; i < 100; i++) {
+            value[i] = value[i-1] + value[i-2];
+        }
+        return value[number-1]; 
+    } else {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+// 1 1 2 3 5 8
